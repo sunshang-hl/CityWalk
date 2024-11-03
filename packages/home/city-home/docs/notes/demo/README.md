11d56428941399232be569a9eae0a9dbc9db1911
松鼠 <hhhelong@126.com>
2024-11-03 17:31:33 +0800

    新增首页Home站点

---
+++ packages/home/city-home/docs/notes/demo/README.md
@@ -0,0 +1,8 @@
+---
+title: Demo
+createTime: 2024/11/01 16:43:01
+permalink: /demo/
+---
+
+- [bar](./bar.md)
+- [foo](./foo.md)
--- shared/config/navbar.ts
+++ shared/config/navbar.ts
@@ -4,7 +4,9 @@ export const zhSharedNavbar = defineNavbarConfig([
   {
     text: '城市',
     items: [
-      {  text: '深圳', link: 'https://shenzhen.citywalk.gronp'}
+      { text: '全国站点', link: 'https://citywalk.gronp' },
+      { text: '深圳', link: 'https://shenzhen.citywalk.gronp' },
+      { text: '广州', link: 'https://guangzhou.citywalk.gronp' }
     ]
   }
 ])
@@ -13,7 +15,9 @@ export const enSharedNavbar = defineNavbarConfig([
   {
     text: 'City',
     items: [
-      {  text: 'ShenZhen', link: 'https://shenzhen.citywalk.gronp'}
+      { text: 'home', link: 'https://citywalk.gronp' },
+      { text: 'ShenZhen', link: 'https://shenzhen.citywalk.gronp' },
+      { text: 'guangzhou', link: 'https://guangzhou.citywalk.gronp' }
     ]
   }
 ])
